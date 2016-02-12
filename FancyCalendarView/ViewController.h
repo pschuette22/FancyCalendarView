295e00249fb9e0bb650d3c484ad93da9e4ae3c57
Pete Schuette <pschuette22@gmail.com>
2016-02-12 10:13:49 -0500

    Initial Commit

---
+++ FancyCalendarView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FancyCalendarView
+//
+//  Created by Peter Schuette on 2/12/16.
+//  Copyright © 2016 Peter Schuette. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FancyCalendarView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FancyCalendarView
+//
+//  Created by Peter Schuette on 2/12/16.
+//  Copyright © 2016 Peter Schuette. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
